951c41b87230ae08945009afbdc60813b7c1ecc1
Kevin White <kevin.white@clubpit.com>
2023-03-05 17:06:58 +0000

    Error decoding JSON payload, maybe switch to linq

---
+++ Orchestrator/Alarm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrator
+{
+    // Alarm myDeserializedClass = JsonConvert.DeserializeObject<Alarm>(myJsonResponse);
+    public class Alarm
+    {
+        public String? AlarmName { get; set; }
+        public String? AlarmDescription { get; set; }
+        public String? AWSAccountId { get; set; }
+        public DateTime AlarmConfigurationUpdatedTimestamp { get; set; }
+        public String? NewStateValue { get; set; }
+        public DateTime StateChangeTime { get; set; }
+        public String? Region { get; set; }
+        public String? AlarmArn { get; set; }
+        public String? OldStateValue { get; set; }
+        public List<String>? OKActions { get; set; }
+        public List<String>? AlarmActions { get; set; }
+        public List<String>? InsufficientDataActions { get; set; }
+        public Trigger? Trigger { get; set; }
+    }
+
+    public class Trigger
+    {
+        public String? MetricName { get; set; }
+        public String? Namespace { get; set; }
+        public String? StatisticType { get; set; }
+        public String? Statistic { get; set; }
+        public String? Unit { get; set; }
+        public List<String>? Dimensions { get; set; }
+        public int Period { get; set; }
+        public int EvaluationPeriods { get; set; }
+        public int DatapointsToAlarm { get; set; }
+        public String? ComparisonOperator { get; set; }
+        public int Threshold { get; set; }
+        public String? TreatMissingData { get; set; }
+        public String? EvaluateLowSampleCountPercentile { get; set; }
+    }
+}
--- Orchestrator/Function.cs
+++ Orchestrator/Function.cs
@@ -3,6 +3,10 @@ using AWS.Lambda.Powertools.Logging;
 using AWS.Lambda.Powertools.Metrics;
 using AWS.Lambda.Powertools.Tracing;
 using Amazon.Lambda.SNSEvents;
+using Amazon.EC2.Model;
+using Amazon.EC2;
+using System.Text.Json;
+using System.Linq.Expressions;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -42,9 +46,62 @@ public class Function
     [Tracing(SegmentName = "ProcessRecordAdync Method")]
     private async Task ProcessRecordAsync(SNSEvent.SNSRecord record, ILambdaContext context)
     {
-        context.Logger.LogInformation($"Processed record {record.Sns.Message}");
+        Logger.LogInformation($"Processed record {record.Sns.Message}");
+        Alarm? alarm = JsonSerializer.Deserialize<Alarm>(record.Sns.Message);
+        Metrics.AddMetric("Alarm Raised", 1, MetricUnit.Count);
+        switch (alarm?.AlarmName)
+        {
+            case "CXM2Veolia":
+                if (alarm.NewStateValue.Equals("ALARM"))
+                {
+                    await RebootInstances("i-0ce23d0464728520f");
+                }
+                break;
+            default:
+                Logger.LogWarning($"Unknown AlarmName : " + alarm?.AlarmName);
+                Metrics.AddMetric("Unknown Alarm", 1, MetricUnit.Count);
+                break;
+        }
+        //await Task.CompletedTask;
+    }
+    [Tracing(SegmentName = "CXM2VeoliaHandler Method")]
+    public async Task CXM2VeoliaHandler(Alarm alarm)
+    {
+        switch (alarm.NewStateValue)
+            {
+            case "ALARM":
+                await RebootInstances("i-0ce23d0464728520f");
+                break;
+            case "OK":
+                Metrics.AddMetric("Alarm Resolved", 1, MetricUnit.Count);
+                break;
+            default:
+                Logger.LogWarning($"Unknown AlarmState : " + alarm.AlarmName + "/" + alarm.NewStateValue);
+                Metrics.AddMetric("Unknown AlarmState", 1, MetricUnit.Count);
+                break;
+            }
+    }
 
-        // TODO: Do interesting work based on the new message
-        await Task.CompletedTask;
+    [Tracing(SegmentName = "RebootInstances Method")]
+    public async Task RebootInstances(string ec2InstanceId)
+    {
+        AmazonEC2Client client = new AmazonEC2Client();
+
+        var request = new RebootInstancesRequest
+        {
+            InstanceIds = new List<string> { ec2InstanceId },
+        };
+
+        var response = await client.RebootInstancesAsync(request);
+        if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            Logger.LogInformation($"{ec2InstanceId} successfully rebooted.");
+            Metrics.AddMetric("Rebooted", 1, MetricUnit.Count);
+        }
+        else
+        {
+            Logger.LogError($"{ec2InstanceId} could not be rebooted");
+            Metrics.AddMetric("Reboot failure", 1, MetricUnit.Count);
+        }
     }
 }
